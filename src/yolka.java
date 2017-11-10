c3392ce75379ce9ba59cf2c9c2592bbdf10ef828
peterbuild <peterbuild@gmail.com>
2017-11-10 17:47:02 +0200

    практикум елка

---
+++ src/yolka.java
@@ -0,0 +1,19 @@
+public class yolka {
+    public static void main(String[] args) {
+        //выводим верхушку, три tab, двойной слеш в конце для спец встаки слеша
+        System.out.println("\t\t\t/\\");
+        //второй ярус, два таба в начале
+        System.out.println("\t\t   /  \\");
+        //третий ярус и т.д..
+        System.out.println("\t\t  / || \\");
+        System.out.println("\t\t /  ||  \\");
+        System.out.println("\t\t/   ||   \\");
+        //дальше нужен только один таб
+        System.out.println("\t   /    ||    \\");
+        System.out.println("\t  /     ||     \\");
+        System.out.println("\t        ||");
+        // выводим почву из знаков =
+        System.out.println("\t ================");
+
+    }
+}
