ea4ed50967c75a74610db44e4be563d740a29426
peterbuild <peterbuild@gmail.com>
2017-11-10 17:12:01 +0200

    немного отредактировал

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args) {
         //
         System.out.println("GoJava10: Petro Ovdienko");
-        System.out.println("Лето провел на работе и ездил на западную Украину))");
-
+        System.out.println("Лето провел на работе в магазине цифровой техники, ездили на западную Украину в город Камянец Подольский))");
     }
 }
