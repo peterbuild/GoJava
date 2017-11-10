579094e204ec91bc70a412aa7e6669c3ee3ae260
peterbuild <peterbuild@gmail.com>
2017-11-10 17:46:43 +0200

    добавил перенос строки

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        //
+        //Выводу строки с использованием System.out.println()
         System.out.println("GoJava10: Petro Ovdienko");
-        System.out.println("Лето провел на работе в магазине цифровой техники, ездили на западную Украину в город Камянец Подольский))");
+        System.out.println("\tЛето провел на работе в магазине цифровой техники, ездили на западную Украину \nв город Камянец Подольский, майнил крипто-валюту)))");
     }
 }
