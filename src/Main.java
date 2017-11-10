773d1befe1e3f25f1a73ef60156e997ce1187982
peterbuild <peterbuild@gmail.com>
2017-11-10 16:48:11 +0200

    добавил домашнее задание как я провел лето

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         //
         System.out.println("GoJava10: Petro Ovdienko");
-        System.out.println("");
+        System.out.println("Лето провел на работе и ездил на западную Украину))");
 
     }
 }
