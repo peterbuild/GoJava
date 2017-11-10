3a19bee0300c1b35ea443efd6e4c7c200776a02c
peterbuild <peterbuild@gmail.com>
2017-11-10 19:17:17 +0200

    оптимизировал код, чтобы файл mario.txt брался из католога src, т.к git папку out не синхрит

--- src/out_txt.java
+++ src/out_txt.java
@@ -1,30 +1,23 @@
 import java.io.*;
-import java.net.URLDecoder;
 
 public class out_txt {
     public static void main(String[] args) throws IOException {
         /* Для выполнения этого задания решил немного пойти вперед и "нагуглил" чтеняе из файла и вывод в консоль,
          т.к. считаю нецелесобразно использовать sout большое количество раз, код выдает требуемое решение задачи))*/
 
-        // получаем путь к файлу
-        File currentClass = new File(URLDecoder.decode(out_txt.class
-                .getProtectionDomain()
-                .getCodeSource()
-                .getLocation()
-                .getPath(), "UTF-8"));
-        // присваиваем это значение переменной classDirectiry
-        String classDirectory = currentClass.getParent();
+        // получаем путь к рабочему каталогу проэкта
+        String classDirectory = System.getProperty("user.dir");
         // проверяем что получается (правильный ли путь)
         //System.out.println(classDirectory);
         // Создаем переменную br для чтения в буффер
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        // создаем переменную или конструктор для объекта f для чтения файла out.txt
-        File f = new File(classDirectory+ "\\out.txt");
+        // создаем переменную или конструктор для объекта f для чтения файла mario.txt
+        File f = new File(classDirectory + "\\src\\mario.txt");
 
         BufferedReader fin = new BufferedReader(new FileReader(f));
         // создаем переменную для строки
         String line;
-            //запучкаем цикл для чтения из файла построчно пока не получим конец файла и выводим сразу же в консоль
+            //запуcкаем цикл для чтения из файла построчно пока не получим конец файла и выводим сразу же в консоль
             while ((line=fin.readLine()) != null) System.out.println(line);
 
     }
